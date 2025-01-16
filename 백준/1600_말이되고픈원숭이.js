02d579a13b3fbbdff9b08d319946236aa1a047b9
Kykim0818 <Kykim@seegene.com>
2025-01-16 20:59:42 +0900

    말이되고픈원숭이 풀이 완

--- 백준/1600_말이되고픈원숭이.js
+++ 백준/1600_말이되고픈원숭이.js
@@ -27,21 +27,18 @@ function solution(multiLineInput) {
     parseInt(v)
   );
   const map = [];
-  const mapInfo = Array.from(Array(mapRow), () =>
-    new Array(mapCol).fill({ moveCnt: -1 })
+  // const mapInfo = Array.from(Array(mapRow), () =>
+  //   new Array(mapCol).fill({ moveCnt: -1 })
+  // );
+  const mapVisit = Array.from(Array(mapRow), () =>
+    Array.from(Array(mapCol), () => new Array(canKnightMoveCnt + 1).fill(-1))
   );
-  const mapVisit = Array.from(Array(canKnightMoveCnt + 1), () =>
-    new Array(mapRow).fill(new Array(mapCol).fill(-1))
-  );
-
-  //   console.log("mapVisit", mapVisit);
 
   for (let i = 0; i < mapRow; i++) {
     map.push(multiLineInput[i + 2].split(" ").map((v) => parseInt(v)));
   }
 
   //   console.log("map", map);
-  //   console.log("mapInfo", mapInfo);
 
   const [startRow, startCol] = [0, 0];
   const [goalRow, goalCol] = [mapRow - 1, mapCol - 1];
@@ -54,21 +51,22 @@ function solution(multiLineInput) {
     kMoveCnt: 0,
   };
   const search = [startInfo];
+  mapVisit[startRow][startCol][0] = 0;
 
   for (let i = 0; i < search.length; i++) {
     const curInfo = search[i];
     const { row, col, moveCnt, kMoveCnt } = curInfo;
-    mapVisit[kMoveCnt][row][col] = moveCnt;
 
     for (let i = 0; i < 4; i++) {
       const mRow = row + rowDir[i];
       const mCol = col + colDir[i];
       if (isInMapIdx(mRow, mCol) && isNotBlock(mRow, mCol)) {
-        if (mapVisit[kMoveCnt][mRow][mCol] === -1) {
+        if (mapVisit[mRow][mCol][kMoveCnt] === -1) {
+          mapVisit[mRow][mCol][kMoveCnt] = moveCnt + 1;
           search.push({
             row: mRow,
             col: mCol,
-            moveCnt: mapVisit[kMoveCnt][row][col] + 1,
+            moveCnt: moveCnt + 1,
             kMoveCnt,
           });
         }
@@ -80,11 +78,12 @@ function solution(multiLineInput) {
         const mRow = row + kRowDir[i];
         const mCol = col + kColDir[i];
         if (isInMapIdx(mRow, mCol) && isNotBlock(mRow, mCol)) {
-          if (mapVisit[kMoveCnt][mRow][mCol] === -1) {
+          if (mapVisit[mRow][mCol][kMoveCnt + 1] === -1) {
+            mapVisit[mRow][mCol][kMoveCnt + 1] = moveCnt + 1;
             search.push({
               row: mRow,
               col: mCol,
-              moveCnt: mapVisit[kMoveCnt][row][col] + 1,
+              moveCnt: moveCnt + 1,
               kMoveCnt: kMoveCnt + 1,
             });
           }
@@ -95,12 +94,15 @@ function solution(multiLineInput) {
 
   const goalMoveCnts = [];
   for (let i = 0; i <= canKnightMoveCnt; i++) {
-    goalMoveCnts.push(mapVisit[i][goalRow][goalCol]);
+    if (mapVisit[goalRow][goalCol][i] !== -1) {
+      goalMoveCnts.push(mapVisit[goalRow][goalCol][i]);
+    }
   }
+  const answer = goalMoveCnts.length === 0 ? -1 : Math.min(...goalMoveCnts);
 
-  console.log("mapVisit", mapVisit[goalRow][goalCol]);
-  console.log("answer", goalMoveCnts);
-  //  return answer;
+  // console.log("mapVisit", mapVisit);
+  console.log("answer", answer);
+  // return answer;
 
   function isInMapIdx(row, col) {
     return row >= 0 && col >= 0 && row < mapRow && col < mapCol;
